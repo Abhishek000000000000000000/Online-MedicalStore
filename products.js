b65478880c200b7460ec7f05e1fdd2320c07efb4
anxhubtw <himanshuthakurapps@gmail.com>
2023-08-10 15:29:31 +0530

    Product page

---
+++ products.js
@@ -0,0 +1,200 @@
+let data = [{
+    name : "Digene Acidity & Gas Relief Gel Orange",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/cropped/pf3vraq88havppemxjar/digene-acidity-gas-relief-gel-orange.png",
+    price : 184,
+    problem : "Acidity",
+    product_form : "liquid"
+},
+{
+    name : "Zingavita Omega 3 Fish Oil 1000mg for Brain & Joint Health | Soft Gelatin Capsule",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/069c103e240f447192f161494b78bf27/zingavita-omega-3-fish-oil-1000mg-for-brain-joint-health-soft-gelatin-capsule.jpg",
+    price : 499,
+    problem : "Immunity",
+    product_form : "tablets"
+},
+{
+    name : "Zingavita Multivitamin for Women with Iron | For Immunity, Hair, Skin & Bones | Tablet",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/797cfa2009814876b60393c3704f8157/zingavita-multivitamin-for-women-with-iron-for-immunity-hair-skin-bones-tablet.jpg",
+    price : 349,
+    problem : "Immunity",
+    product_form : "tablets"
+},
+{
+    name : "Dr Morepen Gluco One BG 03 Blood Glucose Test Strip (Only Strips)",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/pc0kfczcu5phbkk5sy41/dr-morepen-gluco-one-bg-03-blood-glucose-test-strip-only-strips.jpg",
+    price : 628,
+    problem : "Diabetes",
+    product_form : "strips"
+},
+{
+    name : "Tata 1mg Senior 50+ Multivitamin & Multimineral Veg Tablet with Zinc, Vitamin C, Calcium and Vitamin D",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/91892450ab044061bdbfaeafe096dd45/tata-1mg-senior-50-multivitamin-multimineral-veg-tablet-with-zinc-vitamin-c-calcium-and-vitamin-d-supports-immunity-strength-overall-health.jpg",
+    price : 458,
+    problem : "Immunity",
+    product_form : "tablets"
+},
+{
+    name : "Wellbeing Nutrition Melts Vegan Vitamin B12 + Folate | Oral Thin Strip for Energy, Brain & Nerve Function",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/52a11609a2324198911e1380bc36c363/wellbeing-nutrition-melts-vegan-vitamin-b12-folate-oral-thin-strip-for-energy-brain-nerve-function-sugar-free-flavour-misty-orange-mint.jpg",
+    price : 599,
+    problem : "Immunity",
+    product_form : "strips"
+},
+{
+    name : "Sugar Free Gold Low Calorie Aspartame Sweetener",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/cropped/bpgstknem0udqzp9p4dm/sugar-free-gold-low-calorie-aspartame-sweetener.jpg",
+    price : 285,
+    problem : "Diabetes",
+    product_form : "tablets"
+},
+{
+    name : "Cetaphil Moisturising Lotion with Avocado Oil, Vitamin E, B3 & B5",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/071bac94061c4631af205d207e7cd0de/cetaphil-moisturising-lotion-with-avocado-oil-vitamin-e-b3-b5-for-normal-to-combination-sensitive-skin.jpg",
+    price : 889,
+    problem : "Immunity",
+    product_form : "cream"
+},
+{
+    name : "Tata 1mg Blood Pressure Monitor Fully Automatic, Digital BP Monitor",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/c11f1fe8849148c3a30cc7ca1581d24b/tata-1mg-blood-pressure-monitor-fully-automatic-digital-bp-monitor.jpg",
+    price : 1425,
+    problem : "Diabetes",
+    product_form : "digital_device"
+},
+{
+    name : "Plum Mandarin & Vitamin C 15% Face Serum",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/uasrztrr4pej8lyoj9lq/plum-mandarin-vitamin-c-15-face-serum.jpg",
+    price : 593,
+    problem : "Immunity",
+    product_form : "cream"
+},
+{
+    name : "Episoft AC Sunscreen Cream",
+    image : "https://onemg.gumlet.io/images/w_150,f_auto,q_auto,c_fit,h_150/1409cabe63d64119958b2565420d02f4/episoft-ac-sunscreen-cream.jpg",
+    price : 480,
+    problem : "Immunity",
+    product_form : "cream"
+},
+{
+    name : "Seven Seas Original Cod-Liver Oil Capsule",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/hruygy5lw0ivyqenv9ev/seven-seas-original-cod-liver-oil-capsule.jpg",
+    price : 316,
+    problem : "Immunity",
+    product_form : "tablets"
+},
+{
+    name : "Tata 1mg Salmon Omega 3 Fish Oil Capsule",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/ca4ec8b01a524669a2f5160a51af4403/tata-1mg-salmon-omega-3-fish-oil-capsule.jpg",
+    price : 436,
+    problem : "Immunity",
+    product_form : "tablets"
+},
+{
+    name : "Tata 1mg Apple Cider Vinegar ACV Probiotic Plus",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/q9xnukhrycqpcypv7aeg/tata-1mg-apple-cider-vinegar-acv-probiotic-plus-raw-unfiltered-unpasteurized-with-the-mother.jpg",
+    price : 248,
+    problem : "Immunity",
+    product_form : "liquid"
+},
+{
+    name : "Himalaya Wellness Pure Herbs Gokshura Tablet",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/e00734c5e7aa434d92083227c65b98c2/himalaya-wellness-pure-herbs-gokshura-tablet.jpg",
+    price : 196,
+    problem : "Immunity",
+    product_form : "tablets"
+},
+{
+    name : "Tetmosol Medicated Soap with 5% Monosulfiram for Skin Infections | Raw, Unfiltered & Unpasteurized with The Mother",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/963edfc93b2f4f319f6f6d08df1cece8/tetmosol-medicated-soap-with-5-monosulfiram-for-skin-infections.jpg",
+    price : 81,
+    problem : "Immunity",
+    product_form : "soap"
+}]
+
+var filterData1 = data
+var filterData2 = data
+var cart = JSON.parse(localStorage.getItem('cart')) || []
+var wishlist = JSON.parse(localStorage.getItem('wishlist')) || []
+
+showItems(data)
+
+function showItems(data){
+    document.getElementById("container").innerHTML = ""
+    data.map(function(Element){
+        var image = document.createElement("img")
+        image.setAttribute("src", Element.image)
+        var description = document.createElement("p")
+        description.innerHTML = Element.name
+        var price = document.createElement("p")
+        price.innerHTML = "₹" + Element.price
+        var cart = document.createElement("button")
+        cart.innerHTML = "Add to cart"
+        cart.addEventListener("click", function(){
+            addToCart(Element)
+        } )
+        var wl = document.createElement("button")
+        wl.innerHTML = "Add to WishList"
+        wl.addEventListener("click", function(){
+            addToWishlist(Element)
+        } )
+        var child = document.createElement("div")
+        child.setAttribute("id", "child")
+        var childs_child = document.createElement("div")
+        childs_child.setAttribute("id", "childs-child")
+        var container = document.getElementById("container")
+        childs_child.append(cart, wl)
+        child.append(image, description, price, childs_child)
+        container.append(child)
+    })
+}
+
+function addToCart(Element){
+    cart.push(Element)
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+function addToWishlist(Element){
+    wishlist.push(Element)
+    localStorage.setItem("wishlist", JSON.stringify(cart))
+}
+
+let low = document.getElementById("low")
+low.addEventListener("click", lowFuntion)
+
+function lowFuntion(){
+    data = data.sort((a,b) => +a.price - + b.price)
+    console.log(data)
+    showItems(data)
+
+
+}
+
+let high = document.getElementById("high")
+high.addEventListener("click", highFuntion)
+
+function highFuntion(){
+    data = data.sort((a,b) => +b.price - + a.price)
+    console.log(data)
+    showItems(data)
+
+}
+
+let problem = document.getElementById("problem")
+problem.addEventListener("change", ()=>{
+    let filtered = filterData2.filter((ele)=>{
+        if(ele.problem == problem.value){
+            return ele
+        }
+    })
+    showItems(filtered)
+})
+
+let pf = document.getElementById("product_form")
+pf.addEventListener("change", ()=>{
+    let filtered = filterData1.filter((ele)=>{
+        if(ele.product_form == pf.value){
+            return ele
+        }
+    })
+    showItems(filtered)
+})
