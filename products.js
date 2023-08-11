f24c8b37a3273b4d7810b255519a5c672b645b57
anxhubtw <himanshuthakurapps@gmail.com>
2023-08-11 11:50:54 +0530

    Pagination added

--- products.js
+++ products.js
@@ -1,4 +1,4 @@
-let data = [{
+let data1 = [{
     name : "Digene Acidity & Gas Relief Gel Orange",
     image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/cropped/pf3vraq88havppemxjar/digene-acidity-gas-relief-gel-orange.png",
     price : 184,
@@ -53,8 +53,8 @@ let data = [{
     price : 889,
     problem : "Immunity",
     product_form : "cream"
-},
-{
+}]
+var data2 = [{
     name : "Tata 1mg Blood Pressure Monitor Fully Automatic, Digital BP Monitor",
     image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/c11f1fe8849148c3a30cc7ca1581d24b/tata-1mg-blood-pressure-monitor-fully-automatic-digital-bp-monitor.jpg",
     price : 1425,
@@ -110,17 +110,153 @@ let data = [{
     problem : "Immunity",
     product_form : "soap"
 }]
+var data3 = [{
+    name : "Saslic DS Foaming Face Wash with Salicylic Acid | pH 5.5",
+    image : "https://onemg.gumlet.io/images/w_150,c_fit,q_auto,f_auto,h_150/cropped/pf3vraq88havppemxjar/digene-acidity-gas-relief-gel-orange.png",
+    price : 405,
+    problem : "Immunity",
+    product_form : "liquid"
+},
+{
+    name : "Himalaya Liv. 52 DS Tablet",
+    image : "https://onemg.gumlet.io/images/w_150,f_auto,q_auto,c_fit,h_150/cropped/m7ywndnc6att5k6bc8cs/himalaya-liv.-52-ds-tablet.jpg",
+    price : 182,
+    problem : "Immunity",
+    product_form : "tablets"
+},
+{
+    name : "Acnemoist Cream",
+    image : "https://onemg.gumlet.io/images/w_150,f_auto,q_auto,c_fit,h_150/cropped/o0474lscplskmtqiin5v/acnemoist-cream.jpg",
+    price : 319,
+    problem : "Immunity",
+    product_form : "cream"
+},
+{
+    name : "Accu-Chek Softclix Lancets (Only Lancets)",
+    image : "https://onemg.gumlet.io/images/w_150,f_auto,q_auto,c_fit,h_150/cropped/dywg0u5bly2owevrd5my/accu-chek-softclix-lancets-only-lancets.jpg",
+    price : 115,
+    problem : "Diabetes",
+    product_form : "strips"
+},
+{
+    name : "Veet Professional Waxing Strips Kit for Dry Skin, 8 Strips | Gel Wax Hair Removal for Women",
+    image : "https://onemg.gumlet.io/images/w_150,h_150,f_auto,q_auto,c_fit/b743150227c54cd1a20d93db1bd21c38/veet-professional-waxing-strips-kit-for-dry-skin-8-strips-gel-wax-hair-removal-for-women-up-to-28-days-of-smoothness-no-wax-heater-or-wax-beans-required-for-normal-skin.jpg",
+    price : 458,
+    problem : "Immunity",
+    product_form : "strips"
+},
+{
+    name : "Hamdard Arq Gulab Pure Gulab Jal/ Rose Water",
+    image : "https://onemg.gumlet.io/images/w_150,h_150,f_auto,q_auto,c_fit/pyy3ia1ieeaijivwc10b/hamdard-arq-gulab-pure-gulab-jal-rose-water.jpg",
+    price : 67,
+    problem : "Immunity",
+    product_form : "liquid"
+},
+{
+    name : "Tata 1mg Vitamin D3 Capsule",
+    image : "https://onemg.gumlet.io/images/q_auto,c_fit,f_auto,w_150,h_150/d3fe87a68c7c4b27a0061dea72209a76/tata-1mg-vitamin-d3-capsule.jpg",
+    price : 286,
+    problem : "Immunity",
+    product_form : "tablets"
+},
+{
+    name : "Cetaphil DAM Daily Advance Ultra-Hydrating Lotion | For Continuously Dry, Sensitive Skin",
+    image : "https://onemg.gumlet.io/images/w_150,h_150,f_auto,q_auto,c_fit/62ee5a32eff740818ad0e4649773fff5/cetaphil-dam-daily-advance-ultra-hydrating-lotion-for-continuously-dry-sensitive-skin.jpg",
+    price : 487,
+    problem : "Immunity",
+    product_form : "cream"
+}]
+// {
+//     name : "Omron MC-246 Thermometer",
+//     image : "https://onemg.gumlet.io/images/w_150,h_150,f_auto,q_auto,c_fit/4508bb7da8484166a3f8b663215a713c/omron-mc-246-thermometer.jpg",
+//     price : 253,
+//     problem : "Immunity",
+//     product_form : "digital_device"
+// },
+// {
+//     name : "Cetaphil Moisturising Cream | For Dry to Normal, Sensitive Skin",
+//     image : "https://onemg.gumlet.io/c_fit,h_150,w_150,q_auto,f_auto/db9bbd37632540daa5f5837edbc0e162.jpg",
+//     price : 893,
+//     problem : "Immunity",
+//     product_form : "cream"
+// },
+// {
+//     name : "Listerine Cool Mint Mouth Wash",
+//     image : "https://onemg.gumlet.io/images/w_150,h_150,f_auto,q_auto,c_fit/f996ebf5-3b63-4fe6-ac43-25fc63d366e5/listerine-cool-mint-mouth-wash.jpeg",
+//     price : 308,
+//     problem : "Immunity",
+//     product_form : "liquid"
+// }
 
-var filterData1 = data
-var filterData2 = data
+//pagination
+const ul = document.querySelector('ul');
+let allPages = 3;
+let liActive;
+let pageLength;
+let page = 1;
+
+function elem(allPages, page){
+    let li = '';
+
+    let beforePages = page - 1;
+    let afterPages = page + 1;
+    
+
+    if(page > 1){
+        li += `<li class="btn" onclick="elem(allPages, ${page-1})" ><i class="fas fa-angle-left"></i></li>`;
+    }
+
+    for (pageLength = beforePages; pageLength <= afterPages; pageLength++){
+
+        if(pageLength > allPages){
+            continue;
+        }
+        if(pageLength == 0){
+            pageLength = pageLength + 1;
+        }
+
+        if(page == pageLength){
+            liActive = 'active';
+        }else{
+            liActive = '';
+        }
+
+        li += `<li class="numb ${liActive}" onclick="elem(allPages, ${pageLength})" ><span class = "active_span">${pageLength}</span></li>`
+    }
+
+    if(page < allPages){
+        li += `<li class="btn" onclick="elem(allPages, ${page+1})" ><i class="fas fa-angle-right"></i></li>`;
+    }
+
+    ul.innerHTML = li;
+    if(page == 1){
+        showItems(data1)
+    }else if(page == 2){
+        showItems(data2)
+    }else{
+        showItems(data3)
+    }
+}
+elem(allPages, page);
+
+const merge = (first, second) => {
+    for(let i=0; i<second.length; i++) {
+      first.push(second[i]);
+    }
+    return first;
+  }
+
+var filterData1 = merge(merge(data1, data2), data3);
+var filterData2 = filterData1
+var sort_data = filterData1;
 var cart = JSON.parse(localStorage.getItem('cart')) || []
 var wishlist = JSON.parse(localStorage.getItem('wishlist')) || []
 
-showItems(data)
+// showItems(data)
 
 function showItems(data){
     document.getElementById("container").innerHTML = ""
-    data.map(function(Element){
+    data.map(function(Element, index){
         var image = document.createElement("img")
         image.setAttribute("src", Element.image)
         var description = document.createElement("p")
@@ -155,27 +291,27 @@ function addToCart(Element){
 
 function addToWishlist(Element){
     wishlist.push(Element)
-    localStorage.setItem("wishlist", JSON.stringify(cart))
+    localStorage.setItem("wishlist", JSON.stringify(wishlist))
 }
 
 let low = document.getElementById("low")
 low.addEventListener("click", lowFuntion)
 
 function lowFuntion(){
-    data = data.sort((a,b) => +a.price - + b.price)
-    console.log(data)
-    showItems(data)
-
-
+    sort_data = sort_data.sort((a,b) => +a.price - + b.price)
+    console.log(sort_data)
+    showItems(sort_data)
+    document.querySelector(".wrapper").innerHTML = ""
 }
 
 let high = document.getElementById("high")
 high.addEventListener("click", highFuntion)
 
 function highFuntion(){
-    data = data.sort((a,b) => +b.price - + a.price)
-    console.log(data)
-    showItems(data)
+    sort_data = sort_data.sort((a,b) => +b.price - + a.price)
+    console.log(sort_data)
+    showItems(sort_data)
+    document.querySelector(".wrapper").innerHTML = ""
 
 }
 
@@ -187,6 +323,7 @@ problem.addEventListener("change", ()=>{
         }
     })
     showItems(filtered)
+    document.querySelector(".wrapper").innerHTML = ""
 })
 
 let pf = document.getElementById("product_form")
@@ -197,4 +334,5 @@ pf.addEventListener("change", ()=>{
         }
     })
     showItems(filtered)
+    document.querySelector(".wrapper").innerHTML = ""
 })
